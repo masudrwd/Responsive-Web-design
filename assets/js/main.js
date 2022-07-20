dd5960d66ae0b891378567fa35c79e05ec436975
Md Majidul Islam Masud <85607318+masudrwd@users.noreply.github.com>
2022-07-20 18:45:30 +0600

    responsive design with mr web designer

---
+++ assets/js/main.js
@@ -0,0 +1,45 @@
+$(document).ready(function () {
+
+    $('.bars').click(function () {
+        $(this).toggleClass('toggle-ico'); // bar icon
+        $('.nav-bar').toggleClass('nav-show-hide'); // mobile nav
+        $('header').toggleClass('header-bg') //header-bg
+    });
+
+    $('.nav--links').click(function () {
+        $('.bars').removeClass('toggle-ico'); // bar icon
+        $('.nav-bar').removeClass('nav-show-hide'); // mobile nav
+        $('header').removeClass('header-bg') // header-bg
+    });
+
+    $(window).on('load scroll', function () {
+        $('.bars').removeClass('toggle-ico'); // bar icon
+        $('.nav-bar').removeClass('nav-show-hide'); // mobile nav
+        $('header').removeClass('header-bg') // header-bg
+
+        if ($(window).scrollTop() > 30) {
+            $('header').removeClass('header-bg'); // header-bg
+            $('header').addClass('bg-for-header');
+        } else {
+            $('header').removeClass('header-bg'); // header-bg
+            $('header').removeClass('bg-for-header'); // main header-bg
+        }
+    });
+
+    $(' .accordion-header').click(function () {
+        $('.accordion .accordion-body').slideUp(0);
+        $(this).next('.accordion-body').slideDown(500);
+
+        $('.accordion .accordion-header span').text('+');
+        $(this).children('span').text('-');
+    });
+
+    var scroll = new SmoothScroll('a[href*="#"]', {
+        speed: 600,
+        speedAsDuration: true
+    });
+
+
+
+
+});
